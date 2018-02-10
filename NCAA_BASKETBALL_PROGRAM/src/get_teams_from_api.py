2799f87de02c227e250257d4bc3e77755efa1b3c
Rmarken5 <marken.git@gmail.com>
2018-02-10 09:21:34 -0500

    loggin

--- NCAA_BASKETBALL_PROGRAM/src/get_teams_from_api.py
+++ NCAA_BASKETBALL_PROGRAM/src/get_teams_from_api.py
@@ -8,7 +8,8 @@ def main():
     games = rl.main()
     picks = []
     f = openFile()
-    teams_from_file = getTeamsFromFile(f)
+    log = openLog()
+    teams_from_file = getTeamsFromFile(f,log)
     teams_from_game = []
     
     for game in games:
@@ -18,17 +19,23 @@ def main():
 
     for team in sorted(teams_from_game):
         if team not in teams_from_file:
+            log.write('Writing team to file: ' + team)
             f.write(team + '\n')
+        else:
+            log.write('Omitting team from file: ' + team)
     f.close()
 def openFile():
     f = open('C:/Users/Ryan/Documents/Python_Workspace/CBAPP/NCAA_BASKETBALL_PROGRAM/misc_files/teams_from_api.txt' ,'a+' )
     return f
-
-def getTeamsFromFile(file):
+def openLog( ):
+    log = open('C:/Users/Ryan/Documents/Python_Workspace/CBAPP/NCAA_BASKETBALL_PROGRAM/misc_files/teams_from_api_log.txt' ,'a+')
+    return log
+def getTeamsFromFile(file,log):
     teams = []
     fl = file.readlines()
 
     for l in fl:
+        log.write('Found in file: ' + l)
         teams.appen(l)
     return teams
 
